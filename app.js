ba8e0f79a2b8138e9141a0ee554c2aa09b7c797c
Govind <govindagarwal24@gmail.com>
2020-11-07 22:12:50 +0530

    added play function in game play. Basic game play done.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const gameBoard = (function () {
-  const gameArray = ["X", "O", "X", "X", "X", "O", "O", "X", "O"];
+  const gameArray = ["", "", "", "", "", "", "", "", ""];
   function checkHorizontal(player) {
     let score = 0;
     for (let i = 0; i < 9; i += 3) {
@@ -46,7 +46,7 @@ const gameBoard = (function () {
   function setMove(boxNo, sign) {
     gameArray[boxNo] = sign;
   }
-  return { playerWon, getGameArray };
+  return { playerWon, getGameArray, setMove };
 })();
 
 const player = function (name, sign) {
@@ -56,17 +56,41 @@ const player = function (name, sign) {
 };
 
 const gamePlay = (function (gameBoard) {
-  gameArray = gameBoard.getGameArray();
-  console.log(gameArray);
+  let gameArray = gameBoard.getGameArray();
+  function updateBoard() {
+    gameArray = gameBoard.getGameArray();
+  }
   function displayGame(boxes) {
     boxes.forEach((box) => {
       box.textContent = gameArray[box.dataset.box];
     });
   }
-  return { displayGame };
+  function setMove(boxNo, sign) {
+    gameBoard.setMove(boxNo, sign);
+    updateBoard();
+    console.log(gameArray);
+  }
+  function changePlayer(currPlayer, player1, player2) {
+    if (currPlayer == player1) currPlayer = player2;
+    else currPlayer = player1;
+    return currPlayer;
+  }
+  function play(boxes, player1, player2) {
+    let currPlayer = player1;
+    displayGame(boxes);
+    boxes.forEach((box) => {
+      box.addEventListener("click", (e) => {
+        setMove(Number(box.dataset.box), currPlayer.sign);
+        console.log(gameBoard.playerWon(currPlayer));
+        currPlayer = changePlayer(currPlayer, player1, player2);
+        displayGame(boxes);
+      });
+    });
+  }
+  return { play };
 })(gameBoard);
 
 let player1 = player("Govind", "X");
 let player2 = player("Computer", "O");
 const boxes = document.querySelectorAll(".box");
-gamePlay.displayGame(boxes);
+gamePlay.play(boxes, player1, player2);
